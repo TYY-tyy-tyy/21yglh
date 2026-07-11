79a65663cd60d02c16f2b1e943350f60ec125d8e
TYY-tyy-tyy <3615442101@qq.com>
2026-07-12 03:33:06 +0800

    auto: 03:33 auto save

--- yanguo基于校赛改了的，能跑副本/project/code/qy_init/Interrupt_Control.c
+++ yanguo基于校赛改了的，能跑副本/project/code/qy_init/Interrupt_Control.c
@@ -152,12 +152,12 @@ void Speed_DecisionMaking(void)
         nowtargetSpeed = my_Speed/10*9;
     }
     
-	else if(White_Column_MID > 110 && (Image_error>=-15 && Image_error<=15))
+	else if(White_Column_MID > 110 )
     {
         pid.Turn_KP = W_T_KP;//20
         nowtargetSpeed = my_Speed;
     }
-	 else if(White_Column_MID > 110 && ((Image_error<=-15 && Image_error>=-30) ||(Image_error<=30 && Image_error>=15)) )
+	 else if((White_Column_MID > 110) && (Image_error<=-18 || Image_error>=18))
     {
         pid.Turn_KP = 60;//20
         nowtargetSpeed = my_Speed *0.9;
@@ -231,6 +231,6 @@ int get_y(int16 x)
     int16 x_val = clamp_x(x);
     int16 abs_x = abs(x_val);
     // ÕûÊý¼ÆËã£ºy = 46 + (3 \* |x|) / 52
-    int16 y = 48+ (4 * abs_x) / 52;//48 
+    int16 y = 52 + (4 * abs_x) / 52;//48 
     return y;
 }
