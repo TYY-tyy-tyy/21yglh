cb26a4cb17fb9451b4f25871ff557296e3bf0fba
TYY-tyy-tyy <3615442101@qq.com>
2026-07-11 23:39:31 +0800

    fix: 修复波形循迹不走弯道的问题 - 交换波形和直道判断优先级

---
+++ yanguo基于校赛改了的，能跑副本 - 副本 (2)/project/code/Camera/Find_Crossroads.c
@@ -0,0 +1,278 @@
+#include "Find_Crossroads.h"
+
+/* 借存储坐标变量 */
+int16 top_L[2] = {0, 0};
+int16 bot_L[2] = {0, 0};
+
+int16 top_R[2] = {0, 0};
+int16 bot_R[2] = {0, 0};
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数简介     找十字
+// 参数说明
+// 返回参数
+// 使用示例
+// 备注信息
+//------------------------------------------------------------------------------------------------------------------
+void Find_Crossroads(void)
+{
+	uint8 i = 0;
+//	if(r_con == 0 && l_con != 0 && 
+//		Right_Lost_Line_count <= 23 && Left_Lost_Line_count <= 5 && 
+//			Right_Lost_Line_count >= 23 && Left_Lost_Line_count >= 5)
+//	{
+//		Right_Patch_Init();
+//	}
+//	if(l_con == 0 && r_con != 0 && 
+//		Right_Lost_Line_count <= 23 && Left_Lost_Line_count <= 5 && 
+//			Right_Lost_Line_count >= 23 && Left_Lost_Line_count >= 5)
+//	{
+//		Left_Patch_Init();
+//	}
+//	if(Left_Lost_Line_count >= 30 && Right_Lost_Line_count < 10)
+//	{
+//		Right_Patch_Init();
+//	}
+//	else if(Right_Lost_Line_count >= 30 && Left_Lost_Line_count < 10)
+//	{
+//		Left_Patch_Init();
+//	}
+//	else if(Left_Lost_Line_count >= 10 && Right_Lost_Line_count >= 10)
+//	{
+//		Lost_connection_left(40,110);
+//	}
+//	return;
+	
+	if(Left_Lost_Line_count >= 15 && Right_Lost_Line_count >= 15)
+	{
+		/* 找左上下角点 */
+        Left_up_Patch = Find_left_up_point(30,100);
+        Left_dowm_Patch = Find_left_dowm_point(100,40);
+
+        /* 找右上下角点 */
+        Right_up_Patch = Find_Right_up_point(30,100);
+        Right_dowm_Patch = Find_Right_dowm_point(100,40);
+
+        /* 第二级优先级判断十字情况 */
+        //若四个点都存在
+        if((Left_up_Patch != 0) && (Right_up_Patch != 0) && (Left_dowm_Patch != 0) && (Right_dowm_Patch != 0))
+        {
+            /* 左下 */
+            bot_L[0] = (int16)Left_Line[Left_dowm_Patch];
+            bot_L[1] = (int16)Left_dowm_Patch;
+
+            /* 左上 */
+            top_L[0] = (int16)Left_Line[Left_up_Patch];
+            top_L[1] = (int16)Left_up_Patch;
+
+            /* 右下 */
+            bot_R[0] = (int16)Right_Line[Right_dowm_Patch];
+            bot_R[1] = (int16)Right_dowm_Patch;
+
+            /* 右上 */
+            top_R[0] = (int16)Right_Line[Right_up_Patch];
+            top_R[1] = (int16)Right_up_Patch;
+
+            /* 画线 */
+            Patch_line_Left(bot_L, top_L);
+            Patch_line_Right(bot_R, top_R);
+        }
+        //若上面两个角点存在
+        else if((Left_up_Patch != 0) && (Right_up_Patch != 0))
+        {
+            /* 左下 */
+            bot_L[0] = Left_Coordinates[118];
+            bot_L[1] = 118;
+
+            /* 左上 */
+            top_L[0] = (int16)Left_Line[Left_up_Patch];
+            top_L[1] = (int16)Left_up_Patch;
+
+            /* 右下 */
+            bot_R[0] = Right_Coordinates[118];
+            bot_R[1] = 118;
+
+            /* 右上 */
+            top_R[0] = (int16)Right_Line[Right_up_Patch];
+            top_R[1] = (int16)Right_up_Patch;
+
+            /* 画线 */
+            Patch_line_Left(bot_L, top_L);
+            Patch_line_Right(bot_R, top_R);
+        }
+        //若左边两个角点存在
+        else if((Left_up_Patch != 0) && (Left_dowm_Patch != 0))
+        {
+            /* 左下 */
+            bot_L[0] = (int16)Left_Line[Left_dowm_Patch];
+            bot_L[1] = (int16)Left_dowm_Patch;
+
+            /* 左上 */
+            top_L[0] = (int16)Left_Line[Left_up_Patch];
+            top_L[1] = (int16)Left_up_Patch;
+
+            /* 画线 */
+            Patch_line_Left(bot_L, top_L);
+        }
+        //若右边两个角点存在
+        else if((Right_up_Patch != 0) && (Right_dowm_Patch != 0))
+        {
+            /* 右下 */
+            bot_R[0] = (int16)Right_Line[Right_dowm_Patch];
+            bot_R[1] = (int16)Right_dowm_Patch;
+
+            /* 右上 */
+            top_R[0] = (int16)Right_Line[Right_up_Patch];
+            top_R[1] = (int16)Right_up_Patch;
+
+            /* 画线 */
+            Patch_line_Right(bot_R, top_R);
+        }
+    }
+//	else
+//	{
+//		for (i = 110; i >= reference_col_farthest; i-=2)
+//		{
+//			if(Right_Line [i] >= MT9V03X_W - 8)
+//			{
+//				Right_Line[i] = Left_Line[i] + Road_Width_Table[i];
+//				image_copy_out[i][Right_Line[i]] = 0;
+//			}
+//		}
+//		
+//		for (i = 110; i >= reference_col_farthest; i-=2)
+//		{
+//			if(Left_Line [i] == 8)
+//			{
+//				Left_Line[i] = Right_Line[i] - Road_Width_Table[i];
+//				image_copy_out[i][Left_Line[i]] = 0;
+//			}
+//		}
+//	}
+//	Patch_line_Right_Init(30,100);
+//	Patch_line_Left_Init(30,100);
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数简介     找十字
+// 参数说明
+// 返回参数
+// 使用示例
+// 备注信息
+//------------------------------------------------------------------------------------------------------------------
+//void Find_Crossroads(void)
+//{
+//    /* 第一级判断丢线是否符合十字 */
+//    if((Left_Lost_Line_count >= 10) && (Right_Lost_Line_count >= 10))
+//    {
+//        /* 找左上下角点 */
+//        Left_up_Patch = Find_left_up_point(30,100);
+//        Left_dowm_Patch = Find_left_dowm_point(100,40);
+
+//        /* 找右上下角点 */
+//        Right_up_Patch = Find_Right_up_point(30,100);
+//        Right_dowm_Patch = Find_Right_dowm_point(100,40);
+
+//        /* 第二级优先级判断十字情况 */
+//        //若四个点都存在
+//        if((Left_up_Patch != 0) && (Right_up_Patch != 0) && (Left_dowm_Patch != 0) && (Right_dowm_Patch != 0))
+//        {
+//            /* 左下 */
+//            bot_L[0] = (int16)Left_Line[Left_dowm_Patch];
+//            bot_L[1] = (int16)Left_dowm_Patch;
+
+//            /* 左上 */
+//            top_L[0] = (int16)Left_Line[Left_up_Patch];
+//            top_L[1] = (int16)Left_up_Patch;
+
+//            /* 右下 */
+//            bot_R[0] = (int16)Right_Line[Right_dowm_Patch];
+//            bot_R[1] = (int16)Right_dowm_Patch;
+
+//            /* 右上 */
+//            top_R[0] = (int16)Right_Line[Right_up_Patch];
+//            top_R[1] = (int16)Right_up_Patch;
+
+//            /* 画线 */
+//            Patch_line_Left(bot_L, top_L);
+//            Patch_line_Right(bot_R, top_R);
+//        }
+//        //若上面两个角点存在
+//        else if((Left_up_Patch != 0) && (Right_up_Patch != 0))
+//        {
+//            /* 左下 */
+//            bot_L[0] = 30;
+//            bot_L[1] = 118;
+
+//            /* 左上 */
+//            top_L[0] = (int16)Left_Line[Left_up_Patch];
+//            top_L[1] = (int16)Left_up_Patch;
+
+//            /* 右下 */
+//            bot_R[0] = 158;
+//            bot_R[1] = 118;
+
+//            /* 右上 */
+//            top_R[0] = (int16)Right_Line[Right_up_Patch];
+//            top_R[1] = (int16)Right_up_Patch;
+
+//            /* 画线 */
+//            Patch_line_Left(bot_L, top_L);
+//            Patch_line_Right(bot_R, top_R);
+//        }
+//        //若左边两个角点存在
+//        else if((Left_up_Patch != 0) && (Left_dowm_Patch != 0))
+//        {
+//            /* 左下 */
+//            bot_L[0] = (int16)Left_Line[Left_dowm_Patch];
+//            bot_L[1] = (int16)Left_dowm_Patch;
+
+//            /* 左上 */
+//            top_L[0] = (int16)Left_Line[Left_up_Patch];
+//            top_L[1] = (int16)Left_up_Patch;
+
+//            /* 画线 */
+//            Patch_line_Left(bot_L, top_L);
+//        }
+//        //若右边两个角点存在
+//        else if((Right_up_Patch != 0) && (Right_dowm_Patch != 0))
+//        {
+//            /* 右下 */
+//            bot_R[0] = (int16)Right_Line[Right_dowm_Patch];
+//            bot_R[1] = (int16)Right_dowm_Patch;
+
+//            /* 右上 */
+//            top_R[0] = (int16)Right_Line[Right_up_Patch];
+//            top_R[1] = (int16)Right_up_Patch;
+
+//            /* 画线 */
+//            Patch_line_Right(bot_R, top_R);
+//        }
+//    }
+
+
+
+////    /* 找到角点并符合丢线要求 */
+////    if((Left_dowm_Patch >= 30) && (Right_dowm_Patch >= 30) && (Left_Lost_Line_count >= 10) && (Right_Lost_Line_count >= 10) && (Find_Left_FLAG == Left_0) && (Find_Right_FLAG == Right_0))
+////    {
+//////        Buzzer_ON();
+////        /* 左下坐标 */
+////        bot_L[0] = (int16)Left_dowm_Patch_x;
+////        bot_L[1] = (int16)Left_dowm_Patch;
+////
+////        top_L[0] = 40;
+////        top_L[1] = 1;
+////
+////        /* 右下坐标 */
+////        bot_R[0] = (int16)Right_dowm_Patch_x;
+////        bot_R[1] = (int16)Right_dowm_Patch;
+////
+////        top_R[0] = 54;
+////        top_R[1] = 1;
+////
+////        /* 画线 */
+////        Patch_line_Left(bot_L, top_L);
+////        Patch_line_Right(bot_R, top_R);
+////    }
+////    else Buzzer_OFF();
+//}
---
+++ yanguo基于校赛改了的，能跑副本 - 副本 (2)/project/code/qy_init/All_Init.c
@@ -0,0 +1,64 @@
+#include "All_Init.h"
+
+void All_Init(void)
+{
+    /* 按键初始化 */
+    Key_Init();
+
+	/* 舵机初始化 */
+    Servo_Init();
+	
+    /* 电机初始化 */
+    Motors_Init();
+
+    /* 无刷初始化 */
+    Blds_Init();
+
+    /* PID初始化 */
+    PID_Init();
+
+    /* TFT屏幕初始化 */
+    tft180_set_dir(TFT180_CROSSWISE); //横屏模式
+    tft180_init();
+
+    /* 编码器初始化 */
+    Encoder_Init();
+
+    /* 蜂鸣器初始化 */
+    Buzzer_Init();
+	
+	/* 激光笔初始化 */
+	laser_init();
+	
+	wireless_uart_init();
+
+    /* 摄像头初始化 */
+    while(1)
+    {
+        if(mt9v03x_init())
+            tft180_show_string(0, 16, "mt9v03x reinit.");
+        else
+            break;
+    }
+//	system_delay_ms(500);
+//	mt9v03x_set_exposure_time(450);
+//	system_delay_ms(500);
+	seekfree_assistant_interface_init(SEEKFREE_ASSISTANT_DEBUG_UART);
+	seekfree_assistant_camera_information_config(SEEKFREE_ASSISTANT_MT9V03X, image_copy_out[0], MT9V03X_W, MT9V03X_H);
+	 
+    /* 陀螺仪初始化 */
+    //陀螺仪初始化
+    #if IMU_TYPE
+         imu963ra_init();
+    #else
+         imu660ra_init();
+    #endif
+
+    /* 中断初始化 */
+	pit_ms_init(TIM1_PIT,10,Interrupt);
+	
+	//陀螺仪数据初始化
+	null_drift_calculate();
+	
+//	pit_us_init(TIM0_PIT,100,QQYY);
+}
